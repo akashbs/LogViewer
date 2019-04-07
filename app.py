21ea98fb62a5f98e97567378b71b7bfb0eb8743e
Akash Singh <akash.b@toppr.com>
2019-04-08 01:46:29 +0530

    changes

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import time
 
+from flask import Flask, render_template
+from flask import request
+from flask_socketio import SocketIO
 from watchdog.events import PatternMatchingEventHandler
 from watchdog.observers import Observer
+
+app = Flask(__name__, static_url_path='')
+app.config['SECRET_KEY'] = 'secret!'
+app.config['FLASK_ENV'] = 'production'
+app.config['FLASK_DEBUG'] = 1
+socketio = SocketIO(app)
 
 
 class LogChangeHandler(PatternMatchingEventHandler):
@@ -11,9 +20,28 @@ class LogChangeHandler(PatternMatchingEventHandler):
         super().__init__(patterns, ignore_patterns, ignore_directories, case_sensitive)
 
     def on_modified(self, event):
+        socketio.emit('my response', event.__dict__)
         print(event.__dict__)
 
-if __name__ == "__main__":
+
+@app.route('/')
+def root():
+    initial_log = []
+    with open("logs/testing.log") as f:
+        for i in f.readlines():
+            initial_log.append(i)
+    return render_template('static.html', initial_log=initial_log)
+
+
+@app.route('/ping')
+def handle_my_custom_event():
+    print(request.args)
+    socketio.emit('my response', "testing response")
+    return ""
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
     log_observer = Observer()
     log_observer.schedule(
         LogChangeHandler(
